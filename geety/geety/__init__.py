b121e0b5b9249790946759540f74ca88fe34036b
Mark <markradin6886@gmail.com>
2026-01-30 23:49:23 +0300

    300125b. Fixed geety component parsing from xml

--- geety/geety/__init__.py
+++ geety/geety/__init__.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+from typing import Optional
+from io import TextIOBase
 from xml.etree.ElementTree import XMLPullParser
 
 
@@ -15,11 +17,11 @@ class Component:
         self.children = children or []
         self.content = content
                     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'<Component {self.tag} [{", ".join([f"{key}={val}" for key, val in self.args.items()])}] with {len(self.children)} children "{self.content}">'
 
 
-def parse_component(file):
+def parse_component(file: TextIOBase) -> Optional[Component]:
     parser = XMLPullParser(['start', 'end'])
     stack = []
     for line in file:
@@ -28,16 +30,16 @@ def parse_component(file):
             if event == 'start':
                 stack.append(Component(
                     tag=elem.tag,
-                    args=elem.attrib,
-                    content=elem.text or ''
+                    args=elem.attrib.copy()
                 ))
             elif event == 'end' and len(stack) > 1:
+                stack[-1].content = elem.text or ''
                 stack[-2].children.append(stack[-1])
                 del stack[-1]
-    return stack[0]
+    return stack[0] if stack else None
 
 
-def print_tree(component, lvl=0):
+def print_tree(component, lvl=0) -> None:
     print(f'{"\t"*lvl}{component}')
     for child in component.children:
         print_tree(child, lvl+1)
